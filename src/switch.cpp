005df60e2a15041140dd37483fddda21d588e491
Jonathan Hong <jonathanhong@arizona.edu>
2025-11-13 15:28:53 -0700

    commitmeintoamentalhospital

---
+++ src/switch.cpp
@@ -0,0 +1,12 @@
+#include "switch.h"
+#include <avr/io.h>
+
+/*
+ * Initializes pull-up resistor on PB3 and sets it into input mode
+ */
+void initSwitchPin(){
+    DDRB &= ~(1 << DDB3);
+    PORTB |= (1 << PORTB3);
+    PCICR |= (1 << PCIE0);
+    PCMSK0 |= (1 << PCINT3);
+}
--- src/switch.h
+++ src/switch.h
@@ -1,4 +1,6 @@
 #ifndef switch_h
 #define switch_h
+
 void initSwitchPin();
+
 #endif
