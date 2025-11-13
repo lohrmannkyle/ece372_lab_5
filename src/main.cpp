cc1bb0e025aa234c2346748f095c2f0eaaeda12c
KyleScharer <KyleScharer@gmail.com>
2025-11-13 15:39:14 -0700

    ???

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,6 @@
 #include "pwm.h"
 #include "switch.h"
 
-<<<<<<< HEAD
-
-
-
-using namespace std;
-
-=======
->>>>>>> e72dbc3472d91ef13ae0dbcefa47bd73ed658e9d
 //switch debounce statemachine
 typedef enum {wait_press, pressed, wait_release, released} debounce_state;
 
