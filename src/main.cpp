7ba9240def0d2259fa8653516890354d7b56eff9
KyleScharer <KyleScharer@gmail.com>
2025-11-13 15:37:04 -0700

    frick

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
 #include <avr/io.h>
 #include "timer.h"
+#include "cpi.h"
+#include "I2C.h"
+#include "pwm.h"
+#include "switch.h"
 
+<<<<<<< HEAD
+
+
+
+using namespace std;
+
+=======
+>>>>>>> e72dbc3472d91ef13ae0dbcefa47bd73ed658e9d
 //switch debounce statemachine
 typedef enum {wait_press, pressed, wait_release, released} debounce_state;
 
