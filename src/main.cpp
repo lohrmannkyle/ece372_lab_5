657cc9e8d7772ae185447a70f02efa82132324d2
Jonathan Hong <115331046+euler0000@users.noreply.github.com>
2025-11-13 14:42:27 -0700

    hello

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
-print("kyle s");
+#include <Arduino.h>
+#include <avr/io.h>
+#include <iostream>
+
+using namespace std;
+int main(){
+    
+    cout << "wowza garbanza" << endl;
+
+
+
+    return 0;
+}
