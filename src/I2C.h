e376345da101557b7070b042b2190b48afda2ae6
Jonathan Hong <jonathanhong@arizona.edu>
2025-11-20 15:08:01 -0700

    transgender i2c

---
+++ src/I2C.h
@@ -0,0 +1,17 @@
+#ifndef I2C_H
+#define I2C_H
+
+
+void InitI2C();
+
+void StartI2C_Trans(unsigned char SLA);
+
+void StopI2C_Trans();
+
+void Write(unsigned char data);
+
+void Read_from(unsigned char SLA, unsigned char MEMADDRESS);
+
+unsigned char Read_data();
+
+#endif
