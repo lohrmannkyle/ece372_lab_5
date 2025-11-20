e0d44d63ef93b9168d35831e49f1b6a479b8a76a
KyleScharer <KyleScharer@gmail.com>
2025-11-20 15:26:07 -0700

    i2cp1

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -1,33 +1,42 @@
 #include "I2C.h"
 #include <avr/io.h>
 
+#define wait_for_completion while (!(TWCR & (1<<TWINT)); //acknowledge flag goes up
 
-InitI2C(){
+void InitI2C(){
+PRR0 &= ~(1 << PRTWI);  //Restart oscilator
+  
+// generate a 100kHz clock rate
+// SCL freq = 16Mhz/(16 + 2(TWBR)*1)
+// Table 24-7 Prescaler value is 1 so TWPS0 = 1 and TWPS1 = 0.
+  TWSR &= ~((1 << TWPS0)|(TWPS1));
+  TWBR = 0x48; // bit rate generator = 100k  (TWBR = 42)
+
+  TWCR |= (1 << TWINT | 1 << TWEN); // enable two wire interface
+
+}
+
+void StartI2C_Trans(unsigned char Slave_Address){
 
 
 }
 
-StartI2C_Trans(unsigned char Slave_Address){
+void StopI2C_Trans(){
 
 
 }
 
-StopI2C_Trans(){
+void Write(unsigned char Data){
 
 
 }
 
-Write(unsigned char Data){
+void Read_from(unsigned char Slave_Address, unsigned char MEM_ADDRESS){
 
 
 }
 
-Read_from(unsigned char Slave_Address, unsigned char MEM_ADDRESS){
-
-
-}
-
-Read_data(){
+unsigned char Read_data(){
     unsigned char x;
 
 
