b333dfef6a40537a704e76a6ad1d28df948e5733
Jonathan Hong <jonathanhong@arizona.edu>
2025-11-20 15:28:52 -0700

    i2c_start

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -17,8 +17,8 @@ PRR0 &= ~(1 << PRTWI);  //Restart oscilator
 }
 
 void StartI2C_Trans(unsigned char Slave_Address){
-
-
+    TWCR = ((1 << TWEN) | (1 << TWINT) | (1 << TWSTA));
+    wait_for_completion;
 }
 
 void StopI2C_Trans(){
