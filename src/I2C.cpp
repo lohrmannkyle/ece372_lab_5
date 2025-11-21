a7b68e87c67c45ad924a38039d08025865c25d47
Jonathan Hong <jonathanhong@arizona.edu>
2025-11-20 17:35:42 -0700

    i2c debugs

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -1,16 +1,18 @@
 #include "I2C.h"
 #include <avr/io.h>
+#include <Arduino.h>
 
 #define wait_for_completion while (!(TWCR & (1<<TWINT))); //acknowledge flag goes up
 #define trigger_action (TWCR = (1<<TWINT)|(1<<TWEN)); //sets interrupt and enable bits to 1
 
 void InitI2C(){
+    Serial.println("InitI2C");
     PRR0 &= ~(1 << PRTWI);  //Restart oscilator
   
 // generate a 100kHz clock rate
 // SCL freq = 16Mhz/(16 + 2(TWBR)*1)
 // Table 24-7 Prescaler value is 1 so TWPS0 = 1 and TWPS1 = 0.
-  TWSR &= ~((1 << TWPS0)|(TWPS1));
+  TWSR &= ~((1 << TWPS0)|(1 << TWPS1));
   TWBR = 0x48; // bit rate generator = 100k  (TWBR = 42)
 
   TWCR |= (1 << TWINT | 1 << TWEN); // enable two wire interface
@@ -18,12 +20,14 @@ void InitI2C(){
 }
 
 void StartI2C_Trans(unsigned char Slave_Address){
+    Serial.println("StartI2C");
     TWCR = ((1 << TWEN) | (1 << TWINT) | (1 << TWSTA));
     wait_for_completion;
     TWDR = ((Slave_Address)); 
 }
 
 void StopI2C_Trans(){
+    Serial.println("StopI2C");
     trigger_action;
     TWCR = ((1 << TWEN) | (1 << TWINT) | (1 <<TWSTO));
     wait_for_completion;
@@ -31,6 +35,7 @@ void StopI2C_Trans(){
 }
 
 void Write(unsigned char Slave_Address, unsigned char Data){
+    Serial.println("WriteI2C");
     StartI2C_Trans((Slave_Address<<1)|0xFE);
     trigger_action;
     wait_for_completion;
@@ -40,6 +45,7 @@ void Write(unsigned char Slave_Address, unsigned char Data){
 }
 
 void Read_from(unsigned char Slave_Address, unsigned char MEM_ADDRESS){ //slave address needs to be 7 bit, 8 bit mem address
+    Serial.println("Read_from");
     StartI2C_Trans((Slave_Address<<1)|0xFE); // start write
         trigger_action;
         wait_for_completion;
@@ -54,6 +60,7 @@ void Read_from(unsigned char Slave_Address, unsigned char MEM_ADDRESS){ //slave 
 }
 
 unsigned char Read_data(){
+    Serial.println("Read_data");
     unsigned char x;
         x = TWDR;
     return x;
