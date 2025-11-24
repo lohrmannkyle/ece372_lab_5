fa6359a9098f8a7e290271c04dc9d1fe94e69561
Kyle Scharer <KyleScharer@gmail.com>
2025-11-24 14:37:15 -0700

    attempt4

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -21,7 +21,7 @@ void InitI2C(){
 }
 
 void StartI2C_Trans(unsigned char Slave_Address){
-    //Serial.println("StartI2C");
+    Serial.println("StartI2C");
     TWCR = ((1 << TWEN) | (1 << TWINT) | (1 << TWSTA));
     wait_for_completion;
     //Serial.println(TWSR, HEX);
@@ -35,7 +35,7 @@ void StopI2C_Trans(){
 }
 
 void Write(unsigned char Slave_Address, unsigned char Data){
-    //Serial.println("WriteI2C");
+    Serial.println("WriteI2C");
     StartI2C_Trans((Slave_Address<<1)& 0xFE);
     //Serial.println(TWSR, HEX);
     trigger_action;
@@ -52,7 +52,7 @@ void Read_from(unsigned char Slave_Address, unsigned char MEM_ADDRESS){ //slave 
     //Serial.println(Slave_Address);
     //Serial.println(TWSR, HEX);
     Write(Slave_Address, MEM_ADDRESS);
-     TWCR = ((1<<TWSTA)|(1<<TWINT)|(1<<TWEN));
+     TWCR |= ((1<<TWSTA)|(1<<TWINT)|(1<<TWEN));
     wait_for_completion;
     //StartI2C_Trans((Slave_Address<<1)|0xFE); // start write
         //Serial.println("Read_from 1st start fin");
@@ -64,7 +64,7 @@ void Read_from(unsigned char Slave_Address, unsigned char MEM_ADDRESS){ //slave 
         //wait_for_completion;
     StartI2C_Trans((Slave_Address<<1)| 0x01); //start read
        // Serial.println("Read_from 2nd start fin");
-        TWCR = ((1<<TWEA)|(1<<TWINT)|(1<<TWEN)); // sends ack 
+        TWCR |= ((1<<TWEA)|(1<<TWINT)|(1<<TWEN)); // sends ack 
         wait_for_completion; 
         trigger_action;
         wait_for_completion;
